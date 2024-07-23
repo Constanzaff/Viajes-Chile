30ea7f1e20a3782eef05638c069149a7e7f7a56a
Constanza Fuentes <constanza.aff@gmail.com>
2024-07-22 23:42:09 -0400

    Añadido javaScript mensaje

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,9 @@
-$(document).ready(function(){
-    alert("Bienvenido(a) a Viajes Chile");
+document.addEventListener("DOMContentLoaded", function(){
 
-    $(".btn").click(function(){
-        alert("Mensaje enviado con exito")
-    });
-
-    $(".card-title").click(function(){
-    $(".card-text2").toggle();    
-    });
-
-});
-
-$(document).ready(function(){
-    $(".nav-link").on('click', function(){
-        $(this).css({
-            "font-weight": "bold",
-            "font-size": "20px",
-            "color": "#0022ff"
-        });
-    });
-});
-
-$(document).ready(function(){
-    $("a.nav-link").on('click', function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function(){
-                window.location.hash = hash;
-            });
-        }
-    });
+    const form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        alert("Enviado con exito");
+    })
 });
 
